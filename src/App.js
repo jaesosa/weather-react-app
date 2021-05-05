197eb82248dde8634b354a2cd360a1a12fc983e8
jaesosa <79277120+jaesosa@users.noreply.github.com>
2021-05-04 20:24:22 -0400

    Cleaned up code

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import Weather from "./Weather";
 import Forecast from "./Forecast";
 import Footer from "./Footer";
 
-import ReactAnimatedWeather from 'react-animated-weather';
-
 function App() {
   return (
           <div className="container">
